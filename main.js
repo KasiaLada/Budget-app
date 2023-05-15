1d93201149a2d87091a7f9038de43f7369ffaa30
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-05-15 15:35:49 +0200

    added income and expense function

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-const form = document.querySelector("#incomeForm");
+const incomeForm = document.querySelector("#incomeForm");
+const incomeValue = document.querySelector("#incomeValue");
+const expenseForm = document.querySelector("#expenseForm");
 
-form.addEventListener("submit", (event) => {
+
+
+incomeForm.addEventListener("submit", (event) => {
 	event.preventDefault();
     const incomeTitle = document.querySelector("#incomeTitle");
 	addButtonContainer(incomeTitle.value, "#incomesList");
 });
+incomeForm.addEventListener("submit", event => {
+	event.preventDefault();
+	const incomeValue = document.querySelector("#incomeValue");
+	addButtonContainer(incomeValue.value, "#incomesList");
+});
+
+expenseForm.addEventListener("submit", event => {
+	event.preventDefault();
+	const expenseTitle = document.querySelector("#expenseTitle");
+	addButtonContainer(expenseTitle.value, "#expensesList");
+});
+expenseForm.addEventListener("submit", event => {
+	event.preventDefault();
+	const expenseValue = document.querySelector("#expenseValue");
+	addButtonContainer(expenseValue.value, "#expensesList");
+});
+
 const addButtonContainer = (value, containerId) => {
 	const container = document.querySelector(containerId);
 	const li = document.createElement("li");
