bbea169850fbf9e5fe47f41cdda2929c272125b4
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-06-19 14:47:01 +0200

    creating a sum of expenses

--- main.js
+++ main.js
@@ -1,59 +1,50 @@
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-const INCOME = "INCOME";
-const EXPENSE = "EXPENSE";
-const incomeForm = document.querySelector("#incomeForm");
-const incomeValue = document.querySelector("#incomeValue");
-const expenseForm = document.querySelector("#expenseForm");
-const incomeTitle = document.querySelector("#incomeTitle");
-const expenseTitle = document.querySelector("#expenseTitle");
-const expenseValue = document.querySelector("#expenseValue");
-const incomesValue = document.querySelector("#incomesValue");
-const expensesValue = document.querySelector("#expensesValue");
+const INCOME = 'INCOME';
+const EXPENSE = 'EXPENSE';
+const incomeForm = document.querySelector('#incomeForm');
+const incomeValue = document.querySelector('#incomeValue');
+const expenseForm = document.querySelector('#expenseForm');
+const incomeTitle = document.querySelector('#incomeTitle');
+const expenseTitle = document.querySelector('#expenseTitle');
+const expenseValue = document.querySelector('#expenseValue');
+const incomesValue = document.querySelector('#incomesValue');
+const expensesValue = document.querySelector('#expensesValue');
+const sum = document.querySelector('#sum');
 // const budgetValue = document.querySelector("#budgetValue");
 
 let incomeSum = [];
 let expenseSum = [];
 
-incomeForm.addEventListener("submit", event => {
+incomeForm.addEventListener('submit', (event) => {
 	event.preventDefault();
-	addButtonContainer(
-		incomeTitle.value,
-		incomeValue.value,
-		"#incomesList",
-		INCOME
-	);
+	addButtonContainer(incomeTitle.value, incomeValue.value, '#incomesList', INCOME);
 });
 
-expenseForm.addEventListener("submit", event => {
+expenseForm.addEventListener('submit', (event) => {
 	event.preventDefault();
-	addButtonContainer(
-		expenseTitle.value,
-		expenseValue.value,
-		"#expensesList",
-		EXPENSE
-	);
+	addButtonContainer(expenseTitle.value, expenseValue.value, '#expensesList', EXPENSE);
 });
 
 const addButtonContainer = (title, amount, containerId, mode) => {
 	const container = document.querySelector(containerId);
-	const li = document.createElement("li");
+	const li = document.createElement('li');
 	li.id = uuidv4();
 	// li.textContent = `${title} ${amount}`;
-	const spanTitle = document.createElement("span");
+	const spanTitle = document.createElement('span');
 	spanTitle.textContent = title;
 	li.appendChild(spanTitle);
-	const spanAmount = document.createElement("span");
+	const spanAmount = document.createElement('span');
 	spanAmount.textContent = amount;
 	li.appendChild(spanAmount);
 
-	li.style.listStyle = "none";
-	const editButton = document.createElement("button");
-	editButton.textContent = "Edit";
+	li.style.listStyle = 'none';
+	const editButton = document.createElement('button');
+	editButton.textContent = 'Edit';
 	li.appendChild(editButton);
-	const deleteButton = document.createElement("button");
-	deleteButton.textContent = "Delete";
-	deleteButton.addEventListener("click", () => {
+	const deleteButton = document.createElement('button');
+	deleteButton.textContent = 'Delete';
+	deleteButton.addEventListener('click', () => {
 		if (mode === INCOME) {
 			incomeSum = removeData(incomeSum, li, incomesValue);
 		} else {
@@ -75,33 +66,32 @@ const addButtonContainer = (title, amount, containerId, mode) => {
 
 	// ------------
 
-	editButton.addEventListener("click", () => {
-		spanAmount.setAttribute("contenteditable", true);
-		spanTitle.setAttribute("contenteditable", true);
+	editButton.addEventListener('click', () => {
+		spanAmount.setAttribute('contenteditable', true);
+		spanTitle.setAttribute('contenteditable', true);
 	});
-	spanAmount.addEventListener("input", e => {
+	spanAmount.addEventListener('input', (e) => {
 		console.log(e.target.innerText);
 		console.log(li.id);
 		if (mode === INCOME) {
-		// 	incomeSum = incomeSum.map(item => {
-		// 		if (item.id === li.id) {
-		// 			return { ...item, amount: Number(e.target.innerText) };
-		// 		} else {
-		// 			return item;
-		// 		}
-		// 	});
-		// 	incomesValue.textContent = calculateSum(incomeSum);
-		// }
-		// else {
-		// 	expenseSum = expenseSum.map(item => {
-		// 		if (item.id === li.id) {
-		// 			return { ...item, amount: Number(e.target.innerText) };
-		// 		} else {
-		// 			return item;
-		// 		}
-		// 	});
-		// expensesValue.textContent = calculateSum(expenseSum);
-			
+			// 	incomeSum = incomeSum.map(item => {
+			// 		if (item.id === li.id) {
+			// 			return { ...item, amount: Number(e.target.innerText) };
+			// 		} else {
+			// 			return item;
+			// 		}
+			// 	});
+			// 	incomesValue.textContent = calculateSum(incomeSum);
+			// }
+			// else {
+			// 	expenseSum = expenseSum.map(item => {
+			// 		if (item.id === li.id) {
+			// 			return { ...item, amount: Number(e.target.innerText) };
+			// 		} else {
+			// 			return item;
+			// 		}
+			// 	});
+			// expensesValue.textContent = calculateSum(expenseSum);
 		}
 		if (mode === INCOME) {
 			incomeSum = editData(incomeSum, li, incomesValue, e);
@@ -112,13 +102,12 @@ const addButtonContainer = (title, amount, containerId, mode) => {
 	// -----------
 };
 
-
 const manageData = (sum, sumValue, item) => {
 	sum.push(item);
 	sumValue.textContent = calculateSum(sum);
 };
 const editData = (array, li, sumAllData, e) => {
-	const newArray = array.map(item => {
+	const newArray = array.map((item) => {
 		if (item.id === li.id) {
 			return { ...item, amount: Number(e.target.innerText) };
 		} else {
@@ -127,13 +116,13 @@ const editData = (array, li, sumAllData, e) => {
 	});
 
 	sumAllData.textContent = calculateSum(newArray);
-	return newArray
+	return newArray;
 };
 
 const removeData = (array, li, sum) => {
 	li.remove();
 	const newArray = [
-		...array.filter(item => {
+		...array.filter((item) => {
 			if (item.id !== li.id) {
 				return item;
 			}
@@ -143,8 +132,32 @@ const removeData = (array, li, sum) => {
 	sum.textContent = calculateSum(newArray);
 	return newArray;
 };
-const calculateSum = sum => {
+const calculateSum = (sum) => {
 	return sum.reduce((acc, prev) => {
 		return acc + prev.amount;
 	}, 0);
 };
+
+const calculateAllSum = () => {
+	const incomesSum = calculateSum(incomeSum);
+	const expensesSum = calculateSum(expenseSum);
+	const allSum = incomesSum - expensesSum;
+	if (allSum === 0) {
+		sum.innerText = 'Balance is 0 PLN';
+	}
+	if (allSum > 0) {
+		sum.innerText = `You can stil spend ${allSum}PLN`;
+	} else 
+	sum.innerText = `You spent too much ${allSum} PLN`;
+};
+
+// function subtract(incomesSum, expensesSum)
+// return incomesSum - expensesSum;
+// {
+
+//   let result = subtract(incomesSum - expensesSum);
+
+//   return result(incomesSum, expensesSum);
+
+
+// }
